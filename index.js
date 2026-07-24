28a580b40ca9b2a35e7d56e515dcfb452d317a11
pervezmia <ahmedpervezkabir@gmail.com>
2026-07-24 11:47:39 +0600

    date picker filter added

--- index.js
+++ index.js
@@ -62,261 +62,276 @@ const verifyToken = async (req, res, next) => {
 //     // Connect the client to the server	(optional starting in v4.7)
 //     await client.connect();
 
-    const db = client.db("tutor_booking_system");
-    const tutorCollection = db.collection("tutor_booking_system_Collection");
-    const bookedCollection = db.collection("books");
+const db = client.db("tutor_booking_system");
+const tutorCollection = db.collection("tutor_booking_system_Collection");
+const bookedCollection = db.collection("books");
 
-    app.get("/all-tutors", async (req, res) => {
-      // console.log(req.query);
+app.get("/all-tutors", async (req, res) => {
+  const { search, sortBy, startDate, endDate } = req.query;
 
-      const { search, sortBy } = req.query;
-      // console.log({search,sortBy},"ssssss");
+  let query = {};
 
-      let query = {};
+  if (search) {
+    query.$or = [
+      { subjectName: { $regex: search, $options: "i" } },
+      { tutorName: { $regex: search, $options: "i" } },
+      { institution: { $regex: search, $options: "i" } },
+      { location: { $regex: search, $options: "i" } },
+    ];
+  }
 
-      if (search) {
-        query = {
-          $or: [
-            {
-              subjectName: {
-                $regex: search,
-                $options: "i",
-              },
-            },
-            {
-              tutorName: {
-                $regex: search,
-                $options: "i",
-              },
-            },
-            {
-              institution: {
-                $regex: search,
-                $options: "i",
-              },
-            },
-            {
-              location: {
-                $regex: search,
-                $options: "i",
-              },
-            },
-          ],
-        };
-      }
+  if (startDate && endDate) {
+    const start = new Date(startDate);
+    start.setHours(0, 0, 0, 0);
 
-      let sortOption = {};
-      if(sortBy === "price_low"){
-        sortOption = {hourlyFee: 1};
-      } else if(sortBy === "price_high"){
-        sortOption = {hourlyFee: -1};
-      }
-      // else {
-      //   cursor = await tutorCollection.find();
-      // }
+    const end = new Date(endDate);
+    end.setHours(23, 59, 59, 999);
 
-      const cursor = tutorCollection.find(query).sort(sortOption);
-      const result = await cursor.toArray();
-      // console.log(result);
-      res.send(result);
+    query.sessionStartDate = {
+      $gte: start.toISOString(),
+      $lte: end.toISOString(),
+    };
+  }
+
+  let sortOption = {};
+  if (sortBy === "price_low") {
+    sortOption = { hourlyFee: 1 };
+  } else if (sortBy === "price_high") {
+    sortOption = { hourlyFee: -1 };
+  }
+  // else {
+  //   cursor = await tutorCollection.find();
+  // }
+
+  const cursor = tutorCollection.find(query).sort(sortOption);
+  const result = await cursor.toArray();
+  // console.log(result);
+  res.send(result);
+});
+
+app.get("/popular-tutors", async (req, res) => {
+  const cursor = tutorCollection.find().limit(6);
+  const result = await cursor.toArray();
+  // console.log(result);
+  res.send(result);
+});
+
+app.get("/all-tutors/:tutorId", verifyToken, async (req, res) => {
+  // console.log(req.user);
+  const { tutorId } = req.params;
+  // console.log(tutorId);
+  const query = { _id: new ObjectId(tutorId) };
+  const result = await tutorCollection.findOne(query);
+  res.send(result);
+});
+
+// Add a new tutor (Add Tutor page)
+app.post("/tutors", async (req, res) => {
+  const tutorData = req.body;
+  // console.log(tutorData);
+
+  const result = await tutorCollection.insertOne({
+    ...tutorData,
+    totalSlot: Number(tutorData.totalSlot),
+    hourlyFee: Number(tutorData.hourlyFee),
+    createdAt: new Date(),
+  });
+  res.send(result);
+});
+
+//my-tutors getting n
+app.get("/my-tutors/:email", verifyToken, async (req, res) => {
+  const { email } = req.params;
+
+  if (req.user.email !== email) {
+    return res.status(403).json({ message: "Forbidden access" });
+  }
+
+  const result = await tutorCollection.find({ tutorEmail: email }).toArray();
+
+  res.send(result);
+});
+
+//my-tutor updated n
+app.patch("/tutors/:id", verifyToken, async (req, res) => {
+  const { id } = req.params;
+  const updatedData = req.body;
+
+  const tutor = await tutorCollection.findOne({ _id: new ObjectId(id) });
+
+  if (!tutor) {
+    return res.status(404).json({ message: "Tutor not found" });
+  }
+
+  if (tutor.tutorEmail !== req.user.email) {
+    return res.status(403).json({ message: "Forbidden access" });
+  }
+
+  const result = await tutorCollection.updateOne(
+    { _id: new ObjectId(id) },
+    {
+      $set: {
+        ...updatedData,
+        totalSlot: Number(updatedData.totalSlot),
+        hourlyFee: Number(updatedData.hourlyFee),
+      },
+    },
+  );
+
+  res.send(result);
+});
+
+//my-tutor delete n
+app.delete("/tutors/:id", verifyToken, async (req, res) => {
+  const { id } = req.params;
+
+  const tutor = await tutorCollection.findOne({ _id: new ObjectId(id) });
+
+  if (!tutor) {
+    return res.status(404).json({ message: "Tutor not found" });
+  }
+
+  if (tutor.tutorEmail !== req.user.email) {
+    return res.status(403).json({ message: "Forbidden access" });
+  }
+
+  const result = await tutorCollection.deleteOne({ _id: new ObjectId(id) });
+
+  res.send(result);
+});
+
+//get booked
+app.get("/booked/:studentId", verifyToken, async (req, res) => {
+  const { studentId } = req.params;
+  const result = await bookedCollection
+    .find({ studentId: studentId })
+    .toArray();
+  // console.log(result);
+  res.send(result);
+});
+
+app.patch("/booked/:tutorId", verifyToken, async (req, res) => {
+  const { tutorId } = req.params;
+  const bookedData = req.body;
+
+  const tutor = await tutorCollection.findOne({
+    _id: new ObjectId(tutorId),
+  });
+
+  if (!tutor) {
+    res.status(404).json({ message: "Course not Found!" });
+    return;
+  }
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const sessionDate = tutor.sessionStartDate
+    ? new Date(tutor.sessionStartDate)
+    : null;
+
+  if (sessionDate) sessionDate.setHours(0, 0, 0, 0);
+
+  if (sessionDate && today > sessionDate) {
+    return res.status(400).json({
+      message: "Booking is not available yet for this tutor",
     });
+  }
 
-    app.get("/popular-tutors", async (req, res) => {
-      const cursor = tutorCollection.find().limit(6);
-      const result = await cursor.toArray();
-      // console.log(result);
-      res.send(result);
+  if (tutor.totalSlot <= 0) {
+    return res.status(400).json({
+      message: "This session is fully booked. You can not join at the moment.",
     });
+  }
 
-    app.get("/all-tutors/:tutorId", verifyToken, async (req, res) => {
-      // console.log(req.user);
-      const { tutorId } = req.params;
-      // console.log(tutorId);
-      const query = { _id: new ObjectId(tutorId) };
-      const result = await tutorCollection.findOne(query);
-      res.send(result);
-    });
+  await tutorCollection.updateOne(
+    { _id: new ObjectId(tutorId) },
+    {
+      $inc: { totalSlot: -1 },
+      $set: {
+        lastBookedAt: new Date(),
+      },
+    },
+  );
 
-    // Add a new tutor (Add Tutor page)
-    app.post("/tutors", async (req, res) => {
-      const tutorData = req.body;
-      // console.log(tutorData);
+  const result = await bookedCollection.insertOne({
+    ...bookedData,
+    status: "active",
+    bookedAt: new Date(),
+  });
 
-      const result = await tutorCollection.insertOne({
-        ...tutorData,
-        totalSlot: Number(tutorData.totalSlot),
-        hourlyFee: Number(tutorData.hourlyFee),
-        createdAt: new Date(),
-      });
-      res.send(result);
-    });
+  res.send(result);
+});
 
-    //my-tutors getting n
-    app.get("/my-tutors/:email", verifyToken, async (req, res) => {
-      const { email } = req.params;
+app.patch("/booked/cancel/:id", verifyToken, async (req, res) => {
+  const { id } = req.params;
+  const studentId = req.user.id;
 
-      if (req.user.email !== email) {
-        return res.status(403).json({ message: "Forbidden access" });
-      }
+  const existingBooked = await bookedCollection.findOne({
+    _id: new ObjectId(id),
+    studentId,
+  });
 
-      const result = await tutorCollection
-        .find({ tutorEmail: email })
-        .toArray();
+  if (!existingBooked) {
+    return res.status(404).json({ message: "Booking not found!" });
+  }
 
-      res.send(result);
-    });
+  if (existingBooked.status === "cancelled") {
+    return res.status(400).json({ message: "This booking is already cancelled." });
+  }
 
-    //my-tutor updated n
-    app.patch("/tutors/:id", verifyToken, async (req, res) => {
-      const { id } = req.params;
-      const updatedData = req.body;
+  await bookedCollection.updateOne(
+    { _id: new ObjectId(id) },
+    { $set: { status: "cancelled" } }
+  );
 
-      const tutor = await tutorCollection.findOne({ _id: new ObjectId(id) });
+  const result = await tutorCollection.updateOne(
+    { _id: new ObjectId(existingBooked.tutorId) },
+    { $inc: { totalSlot: 1 } }
+  );
 
-      if (!tutor) {
-        return res.status(404).json({ message: "Tutor not found" });
-      }
+  res.status(200).json({ success: true, result });
+});
 
-      if (tutor.tutorEmail !== req.user.email) {
-        return res.status(403).json({ message: "Forbidden access" });
-      }
+//delete er bodole uporer patch korlam -->/booked/cancel/:tutorId
+// app.delete("/booked/:tutorId", verifyToken, async (req, res) => {
+//   const { tutorId } = req.params;
+//   const studentId = req.user.id;
 
-      const result = await tutorCollection.updateOne(
-        { _id: new ObjectId(id) },
-        {
-          $set: {
-            ...updatedData,
-            totalSlot: Number(updatedData.totalSlot),
-            hourlyFee: Number(updatedData.hourlyFee),
-          },
-        },
-      );
+//   const tutorObjectId = new ObjectId(tutorId);
 
-      res.send(result);
-    });
+//   const existingBooked = await bookedCollection.findOne({
+//     tutorId,
+//     studentId,
+//   });
 
-    //my-tutor delete n
-    app.delete("/tutors/:id", verifyToken, async (req, res) => {
-      const { id } = req.params;
+//   if (!existingBooked) {
+//     return res.status(404).json({ message: "Booked tutor is not found!" });
+//   }
 
-      const tutor = await tutorCollection.findOne({ _id: new ObjectId(id) });
+//   await bookedCollection.deleteOne({
+//     tutorId,
+//     studentId,
+//   });
 
-      if (!tutor) {
-        return res.status(404).json({ message: "Tutor not found" });
-      }
+//   const result = await tutorCollection.updateOne(
+//     { _id: tutorObjectId },
+//     {
+//       $inc: {
+//         // enrollCount: existingBooked ? -1 : 0,
+//         totalSlot: 1,
+//       },
+//     },
+//   );
 
-      if (tutor.tutorEmail !== req.user.email) {
-        return res.status(403).json({ message: "Forbidden access" });
-      }
+//   res.status(200).json({ success: true, result });
 
-      const result = await tutorCollection.deleteOne({ _id: new ObjectId(id) });
+//   // res.send(result);
+// });
 
-      res.send(result);
-    });
-
-    //get booked
-    app.get("/booked/:studentId", verifyToken, async (req, res) => {
-      const { studentId } = req.params;
-      const result = await bookedCollection
-        .find({ studentId: studentId })
-        .toArray();
-      // console.log(result);
-      res.send(result);
-    });
-
-    app.patch("/booked/:tutorId", verifyToken, async (req, res) => {
-      const { tutorId } = req.params;
-      const bookedData = req.body;
-
-      const tutor = await tutorCollection.findOne({
-        _id: new ObjectId(tutorId),
-      });
-
-      if (!tutor) {
-        res.status(404).json({ message: "Course not Found!" });
-        return;
-      }
-
-      const today = new Date();
-      today.setHours(0, 0, 0, 0);
-
-      const sessionDate = tutor.sessionStartDate
-        ? new Date(tutor.sessionStartDate)
-        : null;
-
-      if (sessionDate) sessionDate.setHours(0, 0, 0, 0);
-
-      if (sessionDate && today > sessionDate) {
-        return res.status(400).json({
-          message: "Booking is not available yet for this tutor",
-        });
-      }
-
-      if (tutor.totalSlot <= 0) {
-        return res.status(400).json({
-          message:
-            "This session is fully booked. You can not join at the moment.",
-        });
-      }
-
-      await tutorCollection.updateOne(
-        { _id: new ObjectId(tutorId) },
-        {
-          $inc: { totalSlot: -1 },
-          $set: {
-            lastBookedAt: new Date(),
-          },
-        },
-      );
-
-      const result = await bookedCollection.insertOne({
-        ...bookedData,
-        bookedAt: new Date(),
-      });
-
-      res.send(result);
-    });
-
-    app.delete("/booked/:tutorId", verifyToken, async (req, res) => {
-      const { tutorId } = req.params;
-      const studentId = req.user.id;
-
-      const tutorObjectId = new ObjectId(tutorId);
-
-      const existingBooked = await bookedCollection.findOne({
-        tutorId,
-        studentId,
-      });
-
-      if (!existingBooked) {
-        return res.status(404).json({ message: "Booked tutor is not found!" });
-      }
-
-      await bookedCollection.deleteOne({
-        tutorId,
-        studentId,
-      });
-
-      const result = await tutorCollection.updateOne(
-        { _id: tutorObjectId },
-        {
-          $inc: {
-            // enrollCount: existingBooked ? -1 : 0,
-            totalSlot: 1,
-          },
-        },
-      );
-
-      res.status(200).json({ success: true, result });
-
-      // res.send(result);
-    });
-
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
-    );
+// Send a ping to confirm a successful connection
+// await client.db("admin").command({ ping: 1 });
+console.log("Pinged your deployment. You successfully connected to MongoDB!");
 //   } finally {
 //     // Connection stays open to keep serving requests — do not close here.
 //   }
